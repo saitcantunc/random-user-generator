0de64ff2b5934acd3f41075548baa64d7c8771ef
saitcantunc <tuncsaitcan@gmail.com>
2022-01-26 08:23:40 +0300

    Css done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 const Header = () => {
-	return <div>Header</div>;
+	const searchValue = useRef("");
+
+	useEffect(() => {
+		searchValue.current.focus();
+	}, []);
+
+	return (
+		<div className='container'>
+			<div className='title'>Random User Generator</div>
+			<input
+				type='text'
+				ref={searchValue}
+				placeholder='Search'
+				className='form-control mb-5 mt-1'
+			/>
+		</div>
+	);
 };
 
 export default Header;
